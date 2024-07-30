3ffc7fc1e0e7f5704b522caa0b38b445398c32d3
Akshay <akshaynighot73@gmail.com>
2024-07-30 20:53:04 +0530

    code cleanup

--- app/src/main/java/com/absut/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/absut/weatherapp/ui/MainActivity.kt
@@ -85,17 +85,13 @@ import kotlin.math.roundToInt
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    //private lateinit var binding: ActivityMainBinding
     private val viewModel by viewModels<WeatherViewModel>()
 
-    //private lateinit var weatherAdapter: WeatherAdapter
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-        /*binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)*/
 
         val composeView = ComposeView(this).apply {
             setContent {
@@ -118,66 +114,7 @@ class MainActivity : ComponentActivity() {
                 Manifest.permission.ACCESS_COARSE_LOCATION
             )
         )
-
-        /* weatherAdapter = WeatherAdapter()
-         binding.content.recyclerView.adapter = weatherAdapter
-         binding.content.recyclerView.layoutManager =
-             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-         binding.content.recyclerView.setHasFixedSize(true)*/
-
-        /*viewModel.uiState.observe(this) { state ->
-            binding.errorState.isVisible = state == WeatherUIState.Error()
-            binding.progressCircular.isVisible = state == WeatherUIState.Loading
-            binding.content.content.isVisible =
-                state != WeatherUIState.Error() || state != WeatherUIState.Loading
-            when (state) {
-                is WeatherUIState.Error -> {
-                    binding.content.content.visibility = View.GONE
-                    binding.errorState.visibility = View.VISIBLE
-                    binding.errorMsg.text = state.msg
-                    binding.btRetry.setOnClickListener {
-                        viewModel.loadWeatherInfo()
-                    }
-                }
-
-                WeatherUIState.Loading -> {
-                    binding.progressCircular.visibility = View.VISIBLE
-                    binding.content.content.visibility = View.GONE
-                }
-
-                is WeatherUIState.Success -> {
-                    bind(state.weatherInfo)
-                }
-            }
-        }*/
-
-        /*viewModel.locality.observe(this) {
-            binding.content.txtLocation.text = it
-        }*/
     }
-
-    /* private fun bind(weatherInfo: WeatherInfo?) {
-         weatherInfo?.currentWeatherData?.let { data ->
-             binding.content.apply {
-                 //  txtLocation.text = data.
-                 image.setImageResource(data.weatherType.iconRes)
-                 txtTime.text =
-                     "Today, ${data.time.format(DateTimeFormatter.ofPattern("h a"))}"  //value -> Today, 2 pm
-                 txtTemp.text = "${data.temperatureCelsius.roundToInt()}°" // value -> 25°
-                 txtWeatherType.text = data.weatherType.weatherDesc
-                 txtHumidity.text = "${data.humidity.roundToInt()} %"
-                 txtPressure.text = "${data.pressure.roundToInt()} hpa"
-                 txtWind.text = "${data.windSpeed.roundToInt()} km/h"
-                 binding.content.txtToday.text =
-                     "Today | ${data.time.format(DateTimeFormatter.ofPattern("d MMMM"))}"
-             }
-         }
-
-         weatherInfo?.weatherDataPerDay?.get(0)?.let { data ->
-             weatherAdapter.submitList(data)
-
-         }
-     }*/
 
     @Composable
     fun HomeScreen(modifier: Modifier = Modifier) {
