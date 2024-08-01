482ba57ebadad689971271a81ec0719cc31bebe3
Akshay <akshaynighot73@gmail.com>
2024-08-01 20:29:08 +0530

    ErrorView composable UI enhancement

--- app/src/main/java/com/absut/weatherapp/ui/common/ErrorView.kt
+++ app/src/main/java/com/absut/weatherapp/ui/common/ErrorView.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -26,12 +29,28 @@ fun ErrorView(
     ) {
         Column(
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(24.dp)
         ) {
-            Text(text = text ?: "Something went wrong", style = MaterialTheme.typography.bodyMedium)
+            Text(
+                text = text ?: "Something went wrong",
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center,
+            )
             Button(onClick = onButtonClick, modifier = Modifier.padding(top = 16.dp)) {
                 Text(text = buttonText ?: "Try again")
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun ErrorViewPreview() {
+    Surface {
+        ErrorView(text = "Something went wrong! Please try again.") {
+            //do nothing
+        }
+    }
+
 }
