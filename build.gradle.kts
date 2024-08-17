081eb1921fdb490686295ad5594e3d813a262479
Akshay <akshaynighot73@gmail.com>
2024-08-17 16:39:15 +0530

    migrated to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 
 plugins {
-    id("com.android.application") version "8.4.2" apply false
-    id("com.android.library") version "8.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.10" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
 }
